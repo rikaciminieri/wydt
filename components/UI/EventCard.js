7b918908571051d4f59cb29aa090dde68e4daad4
Rika Ciminieri <rikaciminieri@gmail.com>
2022-04-11 19:00:15 -0700

    Event Cards displaying basic data

---
+++ components/UI/EventCard.js
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+const Card = ({ event }) => {
+  const date = new Date(event.dates.start.dateTime).toDateString()
+
+  return (
+    <div className="max-w-sm overflow-hidden rounded shadow-lg">
+      <Link href={event.url}>
+        <a>
+          <img
+            className="w-full"
+            src={event.images[0].url}
+            alt="Sunset in the mountains"
+          />
+        </a>
+      </Link>
+      <div className="px-6 py-4">
+        <Link href={event.url}>
+          <a className="mb-2 text-xl font-bold">{event.name}</a>
+        </Link>
+        <p className="text-base text-gray-700">
+          {date != 'Invalid Date' ?? <p>Date: {date}</p>}
+        </p>
+        <p className="text-base text-gray-700">
+          Time: {event.dates.start.localTime}
+        </p>
+        <p className="text-base text-gray-700">{event.distance} miles</p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+          {event.classifications[0].genre.name}
+        </span>
+        <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+          {event.classifications[0].segment.name}
+        </span>
+        <span className="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700">
+          {event.classifications[0].subGenre.name}
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import type, { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import EventCard from '../components/UI/EventCard'
 
 const Home = (props) => {
   const { country, city, region, latitude, longitude, data } = props
   const events = data._embedded.events
   console.log(events)
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <h1>What Are YOU Doing Tonight?</h1>
+      <h3>
+        Events in {city}, {region}
+      </h3>
       {events.map((event) => {
-        return <p key={event.id}>{event.name}</p>
+        return (
+          <EventCard key={event.id} event={event} />
+        )
       })}
     </div>
   )
