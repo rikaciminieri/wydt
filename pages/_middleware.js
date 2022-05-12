c0ad089be9e323cc85d195ec4f8fb433d92069fc
Rika Ciminieri <rikaciminieri@gmail.com>
2022-05-12 16:31:24 -0700

    Rewrite url to / when accessing /home

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,19 +1,12 @@
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  const { nextUrl: url, geo } = req
-  console.log(geo)
-  const country = geo.country || 'US'
-  const city = geo.city || 'Diamond Bar'
-  const region = geo.region || 'CA'
-  const lat = geo.latitude || '34.0085'
-  const long = geo.longitude || '-117.8136'
+  const url = req.nextUrl.clone()
+  const { pathname } = url
 
-  url.searchParams.set('country', country)
-  url.searchParams.set('city', city)
-  url.searchParams.set('region', region)
-  url.searchParams.set('latitude', lat)
-  url.searchParams.set('longitude', long)
-
-  return NextResponse.rewrite(url)
+  if (pathname === '/') {
+    url.pathname = '/home'
+    return NextResponse.rewrite(url)
+  }
+  return NextResponse.next()
 }
