f89f9379621c147bdff28091998fe4dad961c7da
Rika Ciminieri <rikaciminieri@gmail.com>
2022-05-16 16:54:26 -0700

    Added responsive breakpoints

--- pages/home/index.js
+++ pages/home/index.js
@@ -31,29 +31,31 @@ const Home = ({ country, city, region, latitude, longitude }) => {
   const events = data._embedded.events
 
   return (
-      <div className="min-h-full">
-        <Header />
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-          <div className="relative z-0 grid h-96 w-full place-items-center overflow-hidden">
-            <Image className="absolute" src={crowdImage} alt="Crowd at concert" />
-            <h1 className="absolute flex flex-col items-center text-4xl font-bold text-white">
-              What are YOU doing tonight?
-              <Button className="m-3">Find your next event</Button>
-            </h1>
-          </div>
-          <h3 className="p-5 pb-0 text-lg font-semibold text-orange-600">
-            Events near {city}, {region}
-          </h3>
-          <div className="grid grid-cols-3 gap-6 p-6">
-            {events.map((event) => {
-              return <EventCard key={event.id} event={event} />
-            })}
-          </div>
-          <div className="flex items-center justify-center">
-            <Button className="max-w-fit">View All</Button>
-          </div>
+    <div className="min-h-full">
+      <Header />
+      <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <div className="relative z-0 grid md:h-96 sm:h-auto w-full place-items-center overflow-hidden">
+          <Image className="absolute" src={crowdImage} alt="Crowd at concert" />
+          <h1 className="absolute flex flex-col items-center font-bold text-white sm:text-2xl md:text-3xl lg:text-4xl">
+            What are YOU doing tonight?
+            <Button className="m-3">
+              <p className='sm:text-xs md:text-base lg:text-xl'>Find your next event</p>
+            </Button>
+          </h1>
+        </div>
+        <h3 className="p-5 pb-0 text-lg font-semibold text-orange-600">
+          Events near {city}, {region}
+        </h3>
+        <div className="gap-6 p-6 sm:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {events.map((event) => {
+            return <EventCard key={event.id} event={event} />
+          })}
+        </div>
+        <div className="flex items-center justify-center">
+          <Button className="max-w-fit">View All</Button>
         </div>
       </div>
+    </div>
   )
 }
 
@@ -62,4 +64,3 @@ export default Home
 export const getServerSideProps = async ({ query }) => ({
   props: query,
 })
-
